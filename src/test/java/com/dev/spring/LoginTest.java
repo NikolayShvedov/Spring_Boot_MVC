8855beffac1b9e6124168a2c67f66cee0ea67ee2
Николай Шведов <shved.nikolay.18@mail.ru>
2022-04-11 13:52:21 +0300

    интеграционное тестирование с Spring Testing

---
+++ src/test/java/com/dev/spring/LoginTest.java
@@ -0,0 +1,65 @@
+package com.dev.spring;
+
+import com.dev.spring.controller.MainController;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestPropertySource("/application-test.properties")
+public class LoginTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private MainController controller;
+
+    @Test
+    public void contextLoads() throws Exception {
+        this.mockMvc.perform(get("/"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Hello, guest")))
+                .andExpect(content().string(containsString("Please, login")));
+    }
+
+    @Test
+    public void accessDeniedTest() throws Exception {
+        this.mockMvc.perform(get("/main"))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("http://localhost/login"));
+    }
+
+    @Test
+    @Sql(value = {"/create-user-before.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    public void correctLoginTest() throws Exception {
+        this.mockMvc.perform(formLogin().user("user1").password("1234"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/"));
+    }
+
+    @Test
+    public void badCredentials() throws Exception {
+        this.mockMvc.perform(post("/login").param("username", "jonh"))
+                .andDo(print())
+                .andExpect(status().isForbidden());
+    }
+}
---
+++ target/test-classes/create-user-before.sql
@@ -0,0 +1,10 @@
+delete from user_role;
+delete from users;
+
+insert into users(id, username, password, active) values
+(1, 'admin', '$2a$08$fNUHI3FnO3cbT6VAcClJOOsIq93f2101ud2RAKiZFAh7Y2h.oFRzC', true),
+(2, 'user1', '$2a$08$fNUHI3FnO3cbT6VAcClJOOsIq93f2101ud2RAKiZFAh7Y2h.oFRzC', true);
+
+insert into user_role(user_id, roles) values
+(1, 'ADMIN'), (1, 'USER'),
+(2, 'USER');
